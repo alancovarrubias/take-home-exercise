f2442ff99eb14a1d2b64b246eae8e01f10c579e6
Alan Covarrubias <alan.cov@icloud.com>
2023-04-26 18:46:13 -0700

    add rspec configuration adding focus ability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 # This adds the root directory of your project to the load path
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', ''))
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+end
