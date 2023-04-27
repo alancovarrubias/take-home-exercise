40e7fba5d50d45ec1bdb2a5d180c83d386f08661
Alan Covarrubias <alan.cov@icloud.com>
2023-04-26 22:29:16 -0700

    add error test cases

--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -1,43 +1,52 @@
 require 'rpn_calculator'
 
 RSpec.describe RpnCalculator do
-  let(:calculator) { RpnCalculator.new }
+  subject { RpnCalculator.new }
+
   describe 'test case #1' do
     it 'should return 5' do
-      res = calculator.parse('5')
-      expect(res).to eq(5)
+      expect(subject.parse('5')).to eq(5)
     end
 
     it 'should return 8' do
-      res = calculator.parse('5', '8')
-      expect(res).to eq(8)
+      expect(subject.parse('5', '8')).to eq(8)
     end
 
     it 'should return 13' do
-      res = calculator.parse('5', '8', '+')
-      expect(res).to eq(13)
+      expect(subject.parse('5', '8', '+')).to eq(13)
     end
   end
+
   describe 'test case #2' do
     it 'should return -13' do
-      res = calculator.parse('5 5 5 8 + + -')
-      expect(res).to eq(-13.0)
+      expect(subject.parse('5 5 5 8 + + -')).to eq(-13.0)
     end
   end
+
   describe 'test case #3' do
     it 'should return 6' do
-      res = calculator.parse('-3', '-2', '*')
-      expect(res).to eq(6)
+      expect(subject.parse('-3', '-2', '*')).to eq(6)
     end
     it 'should return 11' do
-      res = calculator.parse('-3', '-2', '*', '5', '+')
-      expect(res).to eq(11)
+      expect(subject.parse('-3', '-2', '*', '5', '+')).to eq(11)
     end
   end
+
   describe 'test case #4' do
     it 'should return 0.625' do
-      res = calculator.parse('5', '9', '1', '-', '/')
-      expect(res).to eq(0.625)
+      expect(subject.parse('5', '9', '1', '-', '/')).to eq(0.625)
+    end
+  end
+
+  describe 'error cases' do
+    it 'should raise "Divide by zero error" exception' do
+      expect { subject.parse('1', '0', '/') }.to raise_error(RpnException, 'Divide by zero error')
+    end
+    it 'should raise "Invalid expression" exception' do
+      expect { subject.parse('INVALID') }.to raise_error(RpnException, 'Invalid expression')
+    end
+    it 'should raise "Not enough operands" exception' do
+      expect { subject.parse('1', '+') }.to raise_error(RpnException, 'Not enough operands')
     end
   end
 end
