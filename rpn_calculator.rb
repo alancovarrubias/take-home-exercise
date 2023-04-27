a034c175ef10ae7b3a73b5d6525bc33ccfe2c62e
Alan Covarrubias <alan.cov@icloud.com>
2023-04-26 23:08:00 -0700

    make error test cases pass

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,10 +1,46 @@
-class String
-  def integer?
-    to_i.to_s == self
+class RpnException < StandardError
+  def initialize(message = 'My custom exception message')
+    super(message)
+  end
+end
+
+module ExpressionParser
+  INTEGER_REGEX = /\A([-+]?[0-9]+)\z/.freeze
+  FLOAT_REGEX = /\A([-+]?[0-9]*\.?[0-9]+)\z/.freeze
+  OPERATOR_REGEX = %r{[+\-*/]}.freeze
+
+  def parse_expression(expression)
+    case expression
+    when INTEGER_REGEX
+      expression.to_i
+    when FLOAT_REGEX
+      expression.to_f
+    when OPERATOR_REGEX
+      perform_operation(expression)
+    else
+      raise RpnException, 'Invalid expression'
+    end
+  end
+
+  def perform_operation(operator)
+    raise RpnException, 'Not enough operands' if @stack.length < 2
+
+    operand1 = @stack.pop
+    operand2 = @stack.pop
+    if operator == '/'
+      raise RpnException, 'Divide by zero error' if operand1.zero?
+
+      operand1 = operand1.to_f
+      operand2 = operand2.to_f
+    end
+
+    operand2.send(operator, operand1)
   end
 end
 
 class RpnCalculator
+  include ExpressionParser
+
   def initialize
     @stack = []
   end
@@ -15,17 +51,9 @@ class RpnCalculator
   end
 
   def parse_input(input)
-    input.split(' ').each { |element| parse_element(element) }
-  end
-
-  def parse_element(element)
-    result = element.integer? ? element.to_f : perform_operation(element)
-    @stack.push(result)
-  end
-
-  def perform_operation(operator)
-    operand1 = @stack.pop.to_f
-    operand2 = @stack.pop.to_f
-    operand2.send(operator, operand1)
+    input.split(' ').each do |expression|
+      result = parse_expression(expression)
+      @stack.push(result)
+    end
   end
 end
