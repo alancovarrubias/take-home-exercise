ad63e1881b72649d19c7ebe7713b318afba3cdf2
Alan Covarrubias <alan.cov@icloud.com>
2023-04-26 19:07:25 -0700

    add third scenario testing multiplication

--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -3,17 +3,17 @@ require 'rpn_calculator'
 RSpec.describe RpnCalculator do
   let(:calculator) { RpnCalculator.new }
   describe 'test case #1' do
-    it '5 should return 5' do
+    it 'should return 5' do
       res = calculator.parse('5')
       expect(res).to eq(5)
     end
 
-    it '8 should return 8' do
+    it 'should return 8' do
       res = calculator.parse('5', '8')
       expect(res).to eq(8)
     end
 
-    it '+ should return 13' do
+    it 'should return 13' do
       res = calculator.parse('5', '8', '+')
       expect(res).to eq(13)
     end
@@ -22,6 +22,16 @@ RSpec.describe RpnCalculator do
     it 'should return -13' do
       res = calculator.parse('5 5 5 8 + + -')
       expect(res).to eq(-13.0)
+    end
+  end
+  describe 'test case #3' do
+    it 'should return 6' do
+      res = calculator.parse('-3', '-2', '*')
+      expect(res).to eq(6)
+    end
+    it 'should return 11' do
+      res = calculator.parse('-3', '-2', '*', '5', '+')
+      expect(res).to eq(11)
     end
   end
 end
