ebdeb7dec1301cbbe4fb2572f35f768f89616053
Alan Covarrubias <alan.cov@icloud.com>
2023-04-26 18:48:04 -0700

    make second scenario test pass

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -10,11 +10,18 @@ class RpnCalculator
   end
 
   def parse(str)
-    if str.integer?
-      @stack.push(str.to_i)
-      str.to_i
-    else
-      @stack.pop + @stack.pop
-    end
+    str.split(' ').each { |element| parse_element(element) }
+    @stack.last
+  end
+
+  def parse_element(element)
+    result = element.integer? ? element.to_i : perform_operation(element)
+    @stack.push(result)
+  end
+
+  def perform_operation(operator)
+    operand1 = @stack.pop.to_i
+    operand2 = @stack.pop.to_i
+    operand2.send(operator, operand1)
   end
 end
